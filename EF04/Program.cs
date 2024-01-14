120cea3830b8b67cf8a1c8f46c1d8d7317c7e332
ghanayemomar <o.ghanaiem@student.aaup.edu>
2024-01-14 15:13:36 +0200

    Add project files.

---
+++ EF04/Program.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.Extensions.Configuration;
+using NHibernate;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Mapping.ByCode;
+using System;
+using NHibernate.Cfg;
+using NHibernate.Driver;
+using NHibernate.Dialect;
+
+namespace EF04.Shared
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            RetriveData();
+            Console.ReadKey();
+        }
+
+        private static void RetriveData()
+        {
+            using (var session = CreateSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var wallets = session.Query<Wallet>();
+                    foreach (var wallet in wallets)
+                    {
+                        Console.WriteLine(wallet);
+                    }
+                }
+            }
+        }
+
+        private static void RetriveDataById()
+        {
+            using (var session = CreateSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    int walletIdToFind = 2;
+                    var wallet = session.Query<Wallet>().FirstOrDefault(x => x.Id == walletIdToFind);
+                    Console.WriteLine(wallet);
+                }
+            }
+        }
+        private static void InsertWallet()
+        {
+            using (var session = CreateSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var walletToAdd = new Wallet { Holder = "Smith", Balance = 0 };
+                    session.Save(walletToAdd);
+                    Console.WriteLine(walletToAdd);
+                    transaction.Commit();
+
+                }
+            }
+        }
+        private static void UpdateWallet()
+        {
+            using (var session = CreateSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var idToUpdate = 10;
+                    var wallet = session.Get<Wallet>(idToUpdate);
+                    wallet.Balance = 99m;
+                    session.Update(wallet);
+                    Console.WriteLine(wallet);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        private static void DeleteWallet()
+        {
+            using (var session = CreateSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var idToDelete = 10;
+
+                    var wallet = session.Get<Wallet>(idToDelete);
+                    session.Delete(wallet);
+                    transaction.Commit();
+                }
+            }
+        }
+        private static void TransferAndTransactionAndConsistency()
+        {
+            using (var session = CreateSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var idFrom = 3;
+                    var idTo = 2;
+                    var amountToTarnsfer = 1000;
+
+                    var walletFrom = session.Get<Wallet>(idFrom);
+                    var walletTo = session.Get<Wallet>(idTo);
+
+                    walletFrom.Balance -= amountToTarnsfer;
+                    walletTo.Balance += amountToTarnsfer;
+                    session.Update(walletFrom);
+                    session.Update(walletTo);
+                    transaction.Commit();   
+                
+                }
+            }
+        }
+
+
+
+        private static ISession CreateSession()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var constr = config.GetSection("constr").Value;
+
+            var mapper = new ModelMapper();
+
+            //list all of type mappings from assembly
+            mapper.AddMappings(typeof(Wallet).Assembly.ExportedTypes);
+
+            //combile class mapping
+            HbmMapping domainMapping = mapper.CompileMappingForAllExplicitlyAddedEntities();
+
+            //Allow the application to specify prop and mapping documents to be used when creating
+            var hbConfig = new Configuration();
+
+            //setting from app to nhibernate
+            hbConfig.DataBaseIntegration(c =>
+            {
+                //strategy to interact with provider
+                c.Driver<MicrosoftDataSqlClientDriver>();
+
+                //dialect nhibernate uses to build syntaxt to rdbms
+                c.Dialect<MsSql2012Dialect>();
+
+                //connectionString
+                c.ConnectionString = constr;
+
+                // log sql statment to console
+                c.LogSqlInConsole = true;
+
+                //format logged sql statment
+                c.LogFormattedSql = true;
+            });
+            hbConfig.AddMapping(domainMapping);
+
+            //instantitate a new IsessionFactory (use Props , settings and mapping)
+            var sessionFactory = hbConfig.BuildSessionFactory();
+
+            var session = sessionFactory.OpenSession();
+
+            return session;
+        }
+    }
+}
---
+++ EF04/Wallet.cs
@@ -0,0 +1,13 @@
+﻿namespace EF04;
+
+public class Wallet
+{
+    public virtual int Id { get; set; }
+    public virtual string? Holder { get; set; }
+    public virtual decimal Balance { get; set; }
+
+    public override string ToString()
+    {
+        return $"[{Id}] {Holder} ({Balance:C})";
+    }
+}
